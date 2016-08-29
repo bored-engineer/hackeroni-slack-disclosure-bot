1a6aab9983d7b76ca752e99a70350929b074b0c5
Luke Young <luke@hydrantlabs.org>
2016-08-28 21:35:48 -0500

    Remove backfill

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	}
 
 	// Track the time to post reports after (starting at launch time)
-	lastKnownReport := time.Now().Add(time.Hour * -5)
+	lastKnownReport := time.Now()
 
 	// Poll for new hacktivity every 2 minutes
     for range time.Tick(2 * time.Minute) {
