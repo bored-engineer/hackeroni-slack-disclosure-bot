79988b48eaf15b24744978d9ed07f153944b2cb9
Luke Young <luke@bored.engineer>
2019-02-13 01:15:58 -0500

    Migrate to using GraphQL via the hackeroni-ql library as old endpoints are now broken

--- main.go
+++ main.go
@@ -1,205 +1,216 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
-	"net/url"
 	"os"
 	"time"
 
-	hackeroni "github.com/bored-engineer/hackeroni/legacy"
+	"github.com/bored-engineer/hackeroni-ql/h1"
 	slack "github.com/bored-engineer/slack-incoming-webhooks"
-	"github.com/robmccoll/mitlru"
+	"github.com/machinebox/graphql"
 )
 
-// Helper function to reverse a slice of reports
-func reverse(reports []*hackeroni.Report) {
-	for i, j := 0, len(reports)-1; i < j; i, j = i+1, j-1 {
-		reports[i], reports[j] = reports[j], reports[i]
-	}
-}
+// The interval to refresh reports
+const interval = 5 * time.Minute
 
+// This is the query run once on bootstrap to get the security icon and most recent hacktivity item
+var bootstrapQuery = `{
+  team(handle: "security") {
+    profile_picture(size: small)
+  }
+  hacktivity_items(
+    secure_order_by: {
+      field: latest_disclosable_activity_at, 
+      direction: DESC
+    }, 
+    where: {
+      report: {
+        disclosed_at: {
+          _is_null: false
+        }
+       }
+      }, 
+      first: 1
+  ) {
+    edges {
+      node {
+        ... on PubliclyDisclosed {
+          __typename
+          report {
+            disclosed_at
+          }
+        }
+      }
+    }
+  }
+}`
+
+// This query gets all the info we need
+var query = `query($since: DateTime) {
+  hacktivity_items(
+    secure_order_by: {
+      field: latest_disclosable_activity_at, 
+      direction: ASC
+  	}, 
+    where: {
+      report: {
+        disclosed_at: {
+          _gt: $since
+        }
+      }
+    }
+  ) {
+    edges {
+      node {
+        ... on PubliclyDisclosed {
+          __typename
+          severity_rating
+          report {
+          	_id
+            url
+            title
+            substate
+            bounties {
+              awarded_currency
+              awarded_amount
+            }
+            disclosed_at
+          }
+          team {
+            url
+            name
+          	profile_picture(size: large)
+          }
+          reporter {
+            name
+            username
+            url
+          	profile_picture(size: large)
+          }
+        }
+      }
+    }
+  }
+}`
+
+// Entry Point
 func main() {
+	ctx := context.Background()
 
 	// Setup the Slack client
 	api := slack.Client{
 		WebhookURL: os.Getenv("SLACK_WEBHOOK_URL"),
 	}
 
-	// Create a new HackerOne client (legacy)
-	client := hackeroni.NewClient(nil)
+	// Create a new HackerOne client
+	client := graphql.NewClient(h1.GraphQLEndpoint)
 
-	// We need the HackerOne icon so lookup the H1 team (named "security")
-	team, _, err := client.Team.Get("security")
-	if err != nil {
-		log.Fatalf("client.Team.Get failed: %v", err)
+	// Make the bootstrap request and parse the response
+	bootstrapReq := graphql.NewRequest(bootstrapQuery)
+	var bootstrapResp h1.Query
+	if err := client.Run(ctx, bootstrapReq, &bootstrapResp); err != nil {
+		log.Fatal(err)
 	}
 
-	// The interval to refresh reports
-	interval := 2 * time.Minute
-	window := 2 * time.Minute
+	// Extract the security icon
+	// TODO: Technically this could change or expire
+	securityIcon := *bootstrapResp.Team.ProfilePicture
 
-	// Hold a LRU cache of report IDs (100 should be fine)
-	cache := mitlru.NewTTLRUCache(100, interval+window)
+	// Extract the most recent disclosure time
+	disclosedSince := bootstrapResp.HacktivityItems.Edges[0].Node.PubliclyDisclosed.Report.DisclosedAt.Add(time.Second)
 
 	// Poll for new hacktivity every interval
 	for range time.Tick(interval) {
 
-		// Look for all activity since now minus the window
-		updatedSince := time.Now().UTC().Add(-window)
-
-		// List all disclosed Hacktivity since last check starting at page 1
-		opts := hackeroni.HacktivityListOptions{
-			Filter:   hackeroni.HacktivityFilterDisclosed,
-			SortType: hackeroni.HacktivitySortTypeLatestDisclosableActivityAt,
-			Page:     1,
-		}
-		var allReports []*hackeroni.Report
-	PageLoop:
-		for {
-			// Actually list
-			reports, _, err := client.Hacktivity.List(opts)
-			if err != nil {
-				log.Printf("client.Hacktivity.List failed: %v", err)
-				break PageLoop
-			}
-
-			// Add every new report
-			for idx, report := range reports {
-				reportTime := report.LatestDisclosableActivityAt.Time
-				if reportTime.Before(updatedSince) {
-					allReports = append(allReports, reports[0:idx]...)
-					break PageLoop
-				}
-			}
-			allReports = append(allReports, reports...)
-
-			// Paginate
-			opts.Page += 1
+		// Make the request with the "since" value from the last run/bootstrap
+		req := graphql.NewRequest(query)
+		req.Var("since", disclosedSince.String())
+		// Run the query
+		var resp h1.Query
+		if err := client.Run(ctx, req, &resp); err != nil {
+			log.Fatal(err)
 		}
 
-		// Reverse the list of reports to post in chronological order
-		log.Printf("Got list of %d reports", len(allReports))
-		//reverse(allReports)
-		log.Printf("Got reverse list %d of reports", len(allReports))
+		// If we got values, set disclosedAt to the last one (newest)
+		if len(resp.HacktivityItems.Edges) > 0 {
+			lastEdge := resp.HacktivityItems.Edges[len(resp.HacktivityItems.Edges)-1]
+			disclosedSince = lastEdge.Node.PubliclyDisclosed.Report.DisclosedAt.Add(time.Second)
+		}
 
-		// Loop each report
-	ReportLoop:
-		for _, report := range allReports {
-			// Check if we've seen the report already, if so skip it
-			_, seen := cache.Get(*report.ID)
-			if seen {
-				log.Printf("Already seen report: %v", *report.ID)
-				continue
-			}
-
-			// Retrieve the full report
-			fullReport, _, err := client.Report.Get(*report.ID)
-			if err != nil {
-				log.Printf("client.Report.Get failed: %v", err)
-				continue ReportLoop
-			}
-
-			// Lookup the reporter
-			reporter, _, err := client.User.Get(*report.Reporter.Username)
-			if err != nil {
-				log.Printf("client.User.Get failed: %v", err)
-				continue ReportLoop
-			}
-
-			// Resolve the relative URLs
-			authorRelativeLink, err := url.Parse(*report.Reporter.URL)
-			if err != nil {
-				log.Printf("url.Parse failed: %v", err)
-				continue ReportLoop
-			}
-			authorLink := client.BaseURL.ResolveReference(authorRelativeLink).String()
-			reportRelativeLink, err := url.Parse(*report.URL)
-			if err != nil {
-				log.Printf("url.Parse failed: %v", err)
-				continue ReportLoop
-			}
-			reportLink := client.BaseURL.ResolveReference(reportRelativeLink).String()
+		// For each report, post it
+		for _, edge := range resp.HacktivityItems.Edges {
+			pd := edge.Node.PubliclyDisclosed
 
 			// Convert the severity into a human readable value if possible
 			var severity string
-			if report.SeverityRating != nil {
-				switch *report.SeverityRating {
-				case hackeroni.ReportSeverityLow:
+			if pd.SeverityRating != nil {
+				switch *pd.SeverityRating {
+				case h1.SeverityRatingEnumLow:
 					severity = "Low"
-				case hackeroni.ReportSeverityMedium:
+				case h1.SeverityRatingEnumMedium:
 					severity = "Medium"
-				case hackeroni.ReportSeverityHigh:
+				case h1.SeverityRatingEnumHigh:
 					severity = "High"
-				case hackeroni.ReportSeverityCritical:
+				case h1.SeverityRatingEnumCritical:
 					severity = "Critical"
 				}
 			}
 
 			// Build the message attachment
 			attachment := slack.Attachment{
-				AuthorName: *report.Reporter.Username,
-				AuthorLink: authorLink,
-				AuthorIcon: *reporter.ProfilePictureURLs.Best(),
-				Title:      fmt.Sprintf("Report %d: %s", *report.ID, *report.Title),
-				TitleLink:  reportLink,
+				AuthorName: *pd.Reporter.Username,
+				AuthorLink: pd.Reporter.URL.String(),
+				AuthorIcon: *pd.Reporter.ProfilePicture,
+				Title:      fmt.Sprintf("Report %s: %s", *pd.Report.ID_, *pd.Report.Title),
+				TitleLink:  pd.Report.URL.String(),
 				Footer:     "HackerOne Disclosure Bot",
-				FooterIcon: *team.ProfilePictureURLs.Best(),
+				FooterIcon: securityIcon,
 				MarkdownIn: []string{"text", "pretext"},
 			}
 
 			// Add the authors name if we have it
-			if reporter.Name != nil && *reporter.Name != "" {
-				attachment.AuthorName += fmt.Sprintf(" (%s)", *reporter.Name)
+			if pd.Reporter.Name != nil && *pd.Reporter.Name != "" {
+				attachment.AuthorName += fmt.Sprintf(" (%s)", *pd.Reporter.Name)
 			}
 
-			// Loop each summary and add the researchers's summary if it exists
-			for _, summary := range fullReport.Summaries {
-				if summary.Category == nil {
-					continue
-				}
-				if summary.Content == nil {
-					continue
-				}
-				switch *summary.Category {
-				case hackeroni.ReportSummaryCategoryTeam:
-					attachment.Pretext = *summary.Content
-				case hackeroni.ReportSummaryCategoryResearcher:
-					attachment.Text = *summary.Content
-				}
-			}
+			// TODO: Summaries
 
 			// Set the attachment color based on the report state (extracted from CSS)
-			switch *report.Substate {
-			case hackeroni.ReportStateNew:
+			switch *pd.Report.Substate {
+			case string(h1.ReportStateEnumNew):
 				attachment.Color = "#8e44ad"
-			case hackeroni.ReportStateTriaged:
+			case string(h1.ReportStateEnumTriaged):
 				attachment.Color = "#e67e22"
-			case hackeroni.ReportStateResolved:
+			case string(h1.ReportStateEnumResolved):
 				attachment.Color = "#609828"
-			case hackeroni.ReportStateNotApplicable:
+			case string(h1.ReportStateEnumNotApplicable):
 				attachment.Color = "#ce3f4b"
-			case hackeroni.ReportStateInformative:
+			case string(h1.ReportStateEnumInformative):
 				attachment.Color = "#ccc"
-			case hackeroni.ReportStateDuplicate:
+			case string(h1.ReportStateEnumDuplicate):
 				attachment.Color = "#a78260"
-			case hackeroni.ReportStateSpam:
+			case string(h1.ReportStateEnumSpam):
 				attachment.Color = "#555"
 			}
 
 			// If the report has a bounty, add it
-			if fullReport.HasBounty != nil && *fullReport.HasBounty {
-				attachment.Fallback = fmt.Sprintf("%s - ", *fullReport.FormattedBounty)
+			if len(pd.Report.Bounties) > 0 {
+				// TODO: This isn't great if there are multiple bounties
+				bounty := pd.Report.Bounties[0]
+				formattedBounty := *bounty.AwardedAmount + " " + *bounty.AwardedCurrency
+				attachment.Fallback = formattedBounty + " - "
 				attachment.AddField(&slack.Field{
 					Title: "Bounty",
-					Value: *fullReport.FormattedBounty,
+					Value: formattedBounty,
 					Short: true,
 				})
 			}
 
 			// If the report has a severity, add it
 			if severity != "" {
-				attachment.Fallback = fmt.Sprintf("%s %s - ", attachment.Fallback, severity)
+				attachment.Fallback += severity + " - "
 				attachment.AddField(&slack.Field{
 					Title: "Severity",
 					Value: severity,
@@ -207,30 +218,27 @@ func main() {
 				})
 			}
 
-			// Finally append the report URL to the fallback
-			attachment.Fallback += fmt.Sprintf("\"%s\" - %s", *report.Title, reportLink)
+			// Finally append the report title and URL to the fallback
+			attachment.Fallback += "\"" + *pd.Report.Title + "\""
+			attachment.Fallback += " - " + pd.Report.URL.String()
 
 			// If we have the exact time it was disclosed add that
-			if fullReport.DisclosedAt != nil {
-				attachment.Timestamp = fullReport.DisclosedAt.Unix()
+			if pd.Report.DisclosedAt != nil {
+				attachment.Timestamp = pd.Report.DisclosedAt.Unix()
 			}
 
 			// Post the actual message
 			log.Printf("Posting: %v", attachment)
-			err = api.Post(&slack.Payload{
-				Username:    fmt.Sprintf("%s Disclosed", *report.Team.Profile.Name),
-				IconURL:     *report.Team.ProfilePictureURLs.Best(),
+			err := api.Post(&slack.Payload{
+				Username:    *pd.Team.Name + " Disclosed",
+				IconURL:     *pd.Team.ProfilePicture,
 				Attachments: []*slack.Attachment{&attachment},
 			})
 			if err != nil {
 				log.Printf("api.Post failed: %v", err)
-				continue ReportLoop
 			}
 
-			// Add to cache
-			log.Printf("Adding %v to cache", *report.ID)
-			cache.Add(*report.ID, true)
 		}
-	}
 
+	}
 }
